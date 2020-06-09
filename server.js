95385ac210e7c01cc02069938c8991922eab5b8b
Rafael Furtado <rnfurtado@gmail.com>
2020-06-08 23:16:44 -0300

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const io = require('socket.io')(server);
 
 //Configs
 var corOptions = {
-    origin : 'http://localhost:3000',
+    origin : 'https://frontend-cursofullstack.herokuapp.com/',
     optionsSuccessStatus : 200
 }
 
